cb4f1fa7b78bc83e90e543b04ee86d9f5d46f1b7
Mitya <techwipes@gmail.com>
2020-09-21 13:07:56 +0500

    add logger, change registerPage method => add param (Model model), add method registerNewUser

--- src/main/java/ru/batu/todolist/controllers/LoginController.java
+++ src/main/java/ru/batu/todolist/controllers/LoginController.java
@@ -1,11 +1,23 @@
 package ru.batu.todolist.controllers;
 
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.batu.todolist.repr.UserRepr;
+
+import javax.validation.Valid;
+
 
 @Controller
 public class LoginController {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 
     @GetMapping("/login")
     public String loginPage(){
@@ -13,7 +25,13 @@ public class LoginController {
     }
 
     @GetMapping("/register")
-    public String registerPage(){
+    public String registerPage(Model model){
+        model.addAttribute("user" , new UserRepr());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerNewUser(@ModelAttribute("user") @Valid UserRepr userRepr){
         return "register";
     }
 
