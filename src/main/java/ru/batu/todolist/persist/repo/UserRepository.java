25b80fd4d621b526ebe23c82b0be4ac2a85cf65a
Mitya <techwipes@gmail.com>
2020-09-24 15:37:27 +0500

    add method getUserByUsername

--- src/main/java/ru/batu/todolist/persist/repo/UserRepository.java
+++ src/main/java/ru/batu/todolist/persist/repo/UserRepository.java
@@ -5,6 +5,12 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ru.batu.todolist.persist.entity.User;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
+
+    boolean existsByUsername(String username);
+
+    Optional<User> getUserByUsername(String username);
 }
