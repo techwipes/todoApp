211bd1a273cd3fb54e28e5667a843bf2372cc720
Mitya <techwipes@gmail.com>
2020-09-24 15:36:32 +0500

    first init

---
+++ src/main/java/ru/batu/todolist/security/SecurityWebApplicationInitializer.java
@@ -0,0 +1,9 @@
+package ru.batu.todolist.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
