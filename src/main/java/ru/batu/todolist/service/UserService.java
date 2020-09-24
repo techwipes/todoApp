a69cfdec8a5723e8f26b1bc1e3944dfe98ed1315
Mitya <techwipes@gmail.com>
2020-09-24 15:37:57 +0500

    add passwordEncoder in createNewUser method

--- src/main/java/ru/batu/todolist/service/UserService.java
+++ src/main/java/ru/batu/todolist/service/UserService.java
@@ -2,6 +2,7 @@ package ru.batu.todolist.service;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import ru.batu.todolist.persist.entity.User;
 import ru.batu.todolist.persist.repo.UserRepository;
@@ -12,17 +13,21 @@ import javax.transaction.Transactional;
 @Service
 @Transactional
 public class UserService {
+
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    public void createNewUser(UserRepr userRepr){
+
+    public void createNewUser(UserRepr userRepr) {
         User user = new User();
         user.setUsername(userRepr.getUsername());
-        user.setPassword(userRepr.getPassword());
+        user.setPassword(passwordEncoder.encode(userRepr.getPassword()));
         userRepository.save(user);
     }
 }
